bbc751099a0012095940b29a5283c1dd986a522a
André Xavier <andre.macedo@aluno.positivo.edu.br>
2018-09-22 18:53:00 -0300

    Singleton ok

--- SingletonXZ/SingletonXZ/src/com/up/clinica/model/ConnectionSingleton.java
+++ SingletonXZ/SingletonXZ/src/com/up/clinica/model/ConnectionSingleton.java
@@ -4,27 +4,23 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class ConnectionSingleton{
+public class ConnectionSingleton {
 
-	 public static Connection conexaoXZ = null;
-	   
-	    private ConnectionSingleton()
-	    {
-	       
-	    }
-	   
-	    public static Connection getInstance()
-	    {
-	        try {
-	            if(conexaoXZ == null)
-	            {
-	                Class.forName("org.hsqldb.jdbc.JDBCDriver");
-	                conexaoXZ = DriverManager.getConnection("jdbc:hsqldb:hsql://localhost/clinicaveterinariadb", "SA", "");
-	            }  
-	        } catch (Exception e) {
-	            e.printStackTrace();
-	        }
-	        return conexaoXZ;
-	    }
+	public static Connection conexaoXZ = null;
+
+	private ConnectionSingleton() {
+	}
+
+	public static Connection getInstance() {
+		try {
+			if (conexaoXZ == null) {
+				Class.forName("org.hsqldb.jdbc.JDBCDriver");
+				conexaoXZ = DriverManager.getConnection("jdbc:hsqldb:hsql://localhost/clinicaveterinariadb", "SA", "");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return conexaoXZ;
+	}
 
 }
